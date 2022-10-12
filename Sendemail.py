7d6e84bf10b6089f29f08dda925a7d0ddc958840
Santi980923 <santiago.espitia@uptc.edu.co>
2022-10-12 15:44:45 -0500

    Cambios en SMTP

--- Sendemail.py
+++ Sendemail.py
@@ -1,10 +1,9 @@
 import smtplib 
 from email.message import EmailMessage 
 
-
 def enviar(email_destino,mensaje,asunto):
-    email_origen="santiagoespitia@uninorte.edu.co"
-    password="Futbol123."
+    email_origen="santiago.espitia@uptc.edu.co"
+    password="santi1"
     email = EmailMessage()
     email["From"] = email_origen
     email["To"] = email_destino
@@ -12,7 +11,7 @@ def enviar(email_destino,mensaje,asunto):
     email.set_content(mensaje)
 
     # Send Email
-    smtp = smtplib.SMTP("smtp-mail.outlook.com", port=587)
+    smtp = smtplib.SMTP("smtp.gmail.com", port=587)
     smtp.starttls()
     smtp.login(email_origen, password)
     smtp.sendmail(email_origen, email_destino, email.as_string())
